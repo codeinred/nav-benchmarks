eba0a29c35a0451a8f2c0927a315b401ef6d907b
Alecto Irene Perez <perez.cs@pm.me>
2022-02-19 14:32:26 -0700

    Repeat enum instantiation multiple times
    It was difficult to measure the impact of scaling when declenum was only
    used once, so this repeats it's usage 10x for Test0..=Test9 in order to
    better measure scaling
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- input/test.cpp
+++ input/test.cpp
@@ -4,7 +4,27 @@
 #define NUM_VALUES 1
 #define TEST_VALUES E0
 #endif
-declenum(TestEnum, int, TEST_VALUES);
+declenum(Test0, int, TEST_VALUES);
+declenum(Test1, int, TEST_VALUES);
+declenum(Test2, int, TEST_VALUES);
+declenum(Test3, int, TEST_VALUES);
+declenum(Test4, int, TEST_VALUES);
+declenum(Test5, int, TEST_VALUES);
+declenum(Test6, int, TEST_VALUES);
+declenum(Test7, int, TEST_VALUES);
+declenum(Test8, int, TEST_VALUES);
+declenum(Test9, int, TEST_VALUES);
 
-static_assert(NUM_VALUES == meta::enum_traits<TestEnum>::count);
+
+static_assert(NUM_VALUES == meta::enum_traits<Test0>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test1>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test2>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test3>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test4>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test5>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test6>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test7>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test8>::count);
+static_assert(NUM_VALUES == meta::enum_traits<Test9>::count);
+
 int main() {}
